bb46b006ff9e8e9be818533c65ade64223f22852
tattii <yuta.tatti@gmail.com>
2020-01-20 05:15:13 +0900

    upload geojson

--- weathermap-xml/weather-map-update.js
+++ weathermap-xml/weather-map-update.js
@@ -1,8 +1,10 @@
 const request = require('request');
 const xml2js = require('xml2js');
+const moment = require('moment');
 
 const {PubSub} = require('@google-cloud/pubsub');
 const {Storage} = require('@google-cloud/storage');
+const projectId = 'weatherbox-217409';
 const bucketName = 'weather-map';
 
 const xml2geojson = require('./xml2geojson');
@@ -28,9 +30,21 @@ function processXML(url) {
       if (xml.Report.Control[0].Status[0] != '通常') return;
 
       const geojson = xml2geojson.parseXML(xml);
-      console.log(geojson);
+      const filename = getFilename(geojson.meta);
+      console.log(filename, geojson);
+      uploadPublic(filename, geojson);
     });
   });
+}
+
+function getFilename(meta) {
+  const datetime = moment(meta.datetime).format('YYYYMMDDhhmm');
+  const type = {
+    '地上実況図': 'analysis',
+    '地上２４時間予想図': 'forecast/24h',
+    '地上４８時間予想図': 'forecast/48h',
+  };
+  return type[meta.title] + '/' + datetime + '.geojson';
 }
 
 
@@ -52,7 +66,7 @@ function processXML(url) {
 // }
 
 async function uploadPublic(filename, data) {
-  const storage = new Storage();
+  const storage = new Storage({ projectId });
   const file = await storage.bucket(bucketName).file(filename);
   file.save(JSON.stringify(data), {
     contentType: 'application/json',
